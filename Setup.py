853f9ee01c3130e5dd77f9a4472aac06cd239bb6
mvandaele <contactvandaele@gmail.com>
2018-10-27 19:20:59 +0200

    save in case of problem refactorizing of the code

--- Setup.py
+++ Setup.py
@@ -5,5 +5,6 @@ from Utils.GetDataSet import GetDataSet
 if __name__ == '__main__':
     getDataSet = GetDataSet("Data/train.txt")
     features, resultats = getDataSet.getData()
-    nn = NeuralNetwork(features, resultats)
-    nn.feedForward()
+    #print (features, resultats)
+    nn = NeuralNetwork(features[:10], resultats[:10])
+    pred = nn.backpropagation_throught_time()
--- UtilsRNN/NeuralNetwork.py
+++ UtilsRNN/NeuralNetwork.py
@@ -4,6 +4,17 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+def softmax(matrix):
+    exp_matrix = np.exp(matrix)
+    return (exp_matrix / np.sum(exp_matrix))
+
+def tanh(z) :
+    return np.tanh(z)
+
+def tanh_derivative(y):
+    return 1 - np.square(y)
 
 #Calculus of the sigmoid
 def sigmoid(z):
@@ -13,6 +24,7 @@ def sigmoid(z):
 def sigmoid_derivative(y):
     return y * (1.0 - y)
 
+
 #Initialisation of the class (input, output, targets, weights, biais)
 class NeuralNetwork:
     def __init__(self, x, y):
@@ -21,21 +33,53 @@ class NeuralNetwork:
         self.inputdim   = self.input.shape[1]
         self.outputdim   = self.y.shape[1]
         self.recurrentdim = 20
-        self.inputWeight = np.random.rand(self.inputdim,self.recurrentdim)
-        self.outputWeight   = np.random.rand(self.recurrentdim,self.outputdim)
+        self.nbASCII = 255
+        self.inputWeight = np.random.rand(self.recurrentdim, self.nbASCII)
+        self.outputWeight   = np.random.rand(self.nbASCII, self.recurrentdim)
         self.recurrentWeight   = np.random.rand(self.recurrentdim,self.recurrentdim)
 
-        self.inputWeightUpdate =  np.zeros_like(self.inputWeight)
-        self.outputWeightUpdate   =  np.zeros_like(self.outputWeight)
-        self.recurrentWeightUpdate   =  np.zeros_like(self.recurrentWeight)
+
 
     def feedForward(self):
-        output = list()
-        layer_1_values = list()
-        layer_1_values.append(np.zeros(self.recurrentdim))
+        self.layer_1_values = list()
+        self.output_values = list()
+        self.layer_1_values.append(np.zeros(self.recurrentdim))
         for indexLetter in range(len(self.input)):
-            x = self.input[indexLetter]
-            layer_1 = sigmoid(np.dot(x,self.inputWeight) + np.dot(layer_1_values[-1],self.recurrentWeight))
-            output_layer = sigmoid(np.dot(layer_1,self.outputWeight))
-            output.append(output_layer)
-        print (output)
+            inputNormalise = np.zeros(self.nbASCII)
+            inputNormalise[self.input[indexLetter]] = 1
+            layer_1 = np.dot(self.inputWeight, inputNormalise) + np.dot(self.recurrentWeight, self.layer_1_values[-1])
+            layer_1_actif = tanh(layer_1)
+            layer_output = np.dot(self.outputWeight, layer_1)
+            self.layer_1_values.append(layer_1_actif)
+            self.output_values.append(layer_output)
+
+    def prediction(self):
+        self.feedForward()
+        results =  [np.argmax(softmax(layer)) for layer in self.output_values]
+        return results
+
+    def loss_calculation(self):
+        assert len(self.input) == len(self.y)
+        self.feedForward()
+        loss = 0.0
+        for i, layer_output in enumerate(self.output_values):
+            probs = softmax(layer_output)
+            loss += -np.log(probs[int(self.y[i])])
+        return loss/len(self.y)
+
+    def backpropagation_throught_time(self):
+        T = len(self.y)
+        inputWeightUpdate =  np.zeros_like(self.inputWeight)
+        outputWeightUpdate   =  np.zeros_like(self.outputWeight)
+        recurrentWeightUpdate   =  np.zeros_like(self.recurrentWeight)
+        self.feedForward()
+        delta_o = [softmax(layer) for layer in self.output_values]
+
+        for t in range(T):
+            delta_o[t][int(self.y[t])] -= 1.
+
+        for t in np.arange(T)[::-1]:
+            print (np.asarray(delta_o[t]))
+            recurrentWeightUpdate += np.outer(np.asarray(delta_o[t]), np.asarray(self.layer_1_values[t].T))
+
+            #delta_t = self.V.T.dot(delta_o[t]) * (1 - (self.layer_1_values[t] ** 2))
