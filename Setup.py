0673e18d2689398966b2e52b23b266ee62ea9eb3
VANDAELE Mathias Michel Albert <mathias.vandaele@soprasteria.com>
2018-11-02 11:14:50 +0100

    add of bptt and readjuting code

--- Setup.py
+++ Setup.py
@@ -5,5 +5,8 @@ from Utils.GetDataSet import GetDataSet
 if __name__ == '__main__':
     getDataSet = GetDataSet("Data/train.txt")
     features, resultats = getDataSet.getData()
-    nn = NeuralNetwork(features[0], resultats[0])
-    pred = nn.backpropagation_throught_time()
+    nn = NeuralNetwork(features, resultats)
+    print (nn.loss_calculation())
+    for i in range(100):
+        nn.backpropagation_throught_time(features[0], resultats[0])
+    print (nn.loss_calculation())
--- UtilsRNN/NeuralNetwork.py
+++ UtilsRNN/NeuralNetwork.py
@@ -22,48 +22,54 @@ class NeuralNetwork:
         self.outputWeight   = np.random.rand(self.nbASCII, self.recurrentdim)
         self.recurrentWeight   = np.random.rand(self.recurrentdim,self.recurrentdim)
         self.bptt_truncate = 100
+        self.learning_rate = 0.01
 
-    def feedForward(self):
-        iterations = len(self.input)
+    def feedForward(self, x):
+        iterations = len(x)
         layer_1_activated = np.zeros((iterations + 1, self.recurrentdim))
         layer_1_activated[-1] = np.zeros(self.recurrentdim)
         output = np.zeros((iterations, self.nbASCII))
 
         for iteration in np.arange(iterations):
-            layer_1_activated[iteration] = np.tanh(self.inputWeight[:,int(self.input[iteration])] + self.recurrentWeight.dot(layer_1_activated[iteration-1]))
+            layer_1_activated[iteration] = np.tanh(self.inputWeight[:,int(x[iteration])] + self.recurrentWeight.dot(layer_1_activated[iteration-1]))
             output[iteration] = softmax(self.outputWeight.dot(layer_1_activated[iteration]))
         return output, layer_1_activated
 
-    def prediction(self):
-        output, layer_1_activated = self.feedForward()
+    def prediction(self, x):
+        output, layer_1_activated = self.feedForward(x)
         results = np.argmax(output, axis=1)
         return results
 
     def loss_calculation(self):
-        assert len(self.input) == len(self.y)
-        loss = 0.
+        nb_entry = np.sum((len(y_i) for y_i in self.y))
+        loss = 0
         for i in np.arange(len(self.y)):
-            output, layer_1_activated = self.feedForward()
-            correct_word_predictions = output[np.arange(len(self.y[i])), int(self.y[i])]
+            output, layer_1_activated = self.feedForward(self.input[i])
+            correct_word_predictions = output[np.arange(len(self.y[i])), self.y[i]]
             loss += -1 * np.sum(np.log(correct_word_predictions))
-        return loss
+        return loss/nb_entry
 
 
-    def backpropagation_throught_time(self):
-        output, layer_1_activated = self.feedForward()
-        iterations = len(self.y)
+    def backpropagation_throught_time(self, x, y):
+        output, layer_1_activated = self.feedForward(x)
+        iterations = len(y)
 
         inputWeight_update = np.zeros(self.inputWeight.shape)
         outputWeight_update = np.zeros(self.outputWeight.shape)
         recurrentWeight_update = np.zeros(self.recurrentWeight.shape)
 
         delta_o = output
-        print (self.input)
-        print (self.y)
-        delta_o[np.arange(len(self.y)), self.y] -= 1.
+        delta_o[np.arange(len(y)), y] -= 1.
         for iteration in np.arange(iterations)[::-1]:
             outputWeight_update += np.outer(delta_o[iteration], layer_1_activated[iteration].T)
             delta_t = self.outputWeight.T.dot(delta_o[iteration]) * (1 - (layer_1_activated[iteration] ** 2))
 
             for bptt_step in np.arange(max(0, iteration-self.bptt_truncate), iteration+1)[::-1]:
                 recurrentWeight_update += np.outer(delta_t, layer_1_activated[bptt_step-1])
+                inputWeight_update[:,x[bptt_step]] += delta_t
+
+                delta_t = self.recurrentWeight.T.dot(delta_t) * (1 - layer_1_activated[bptt_step-1] ** 2)
+
+        self.inputWeight -= inputWeight_update * self.learning_rate
+        self.recurrentWeight -= recurrentWeight_update * self.learning_rate
+        self.outputWeight -= outputWeight_update * self.learning_rate
